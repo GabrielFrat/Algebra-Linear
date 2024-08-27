f9fae9be851dfdc4afbce928f55bdb97c6bbaa13
Gabriel Fratucci dos Reis <gabrielfrat@hotmail.com>
2024-08-26 22:24:49 -0300

    version 1.0.5

--- linear_algebra.py
+++ linear_algebra.py
@@ -154,6 +154,38 @@ class linear_alg():
         except TypeError:
             print("Error when calculating matrix results")
 
+
+    def det(self, matrix):
+        num_linhas_a, num_colunas_a = len(matrix), len(matrix[0])
+
+        try:
+            assert num_linhas_a == num_colunas_a
+        except TypeError:
+            print("The matrices must be the same size")
+        
+        if num_linhas_a == 2:
+            return (matrix[0][0] * matrix[1][1]) - (matrix[0][1] * matrix[1][0])
+        else:
+            for i in range(0, num_linhas_a):
+                aux = matrix[i][:-1]
+
+                for value in aux:
+                    matrix[i].append(value)
+
+        det = ((matrix[0][0] * matrix[1][1] * matrix[2][2]) + (matrix[0][1]  
+                    * matrix[1][2] * matrix[2][0]) + (matrix[0][2] * matrix[1][0] * matrix[2][1])) - ((matrix[2][0] 
+                    * matrix[1][1] * matrix[0][2]) + (matrix[2][1]  * matrix[1][2] * matrix[0][0]) + (matrix[2][2] 
+                    * matrix[1][0] * matrix[0][1]))
+        return det
+
+    def inverse_matrix(self, matrix):
+
+        def verify(matrix):
+            pass
+
+        def function(matrix):
+            pass
+
 # Vector sum test
 algebra = linear_alg()
 soma = algebra.sum_vector([2, 1], [3, 1], [4, 2])
@@ -166,3 +198,7 @@ soma_matriz = algebra.product_matrix([[1, 2], [2, 1]], [[2, 4], [4, 4]])
 escalar_matriz = algebra.scalar_matrix(2, [[1, 2], [1, 3]])
 soma_matriz = algebra.sum_matrix([[1, 2], [2, 1], [2, 5]], [[2, 4], [4, 4], [2, 7]])
 sub_matriz = algebra.subtract_matrix([[1, 2], [2, 1], [2, 5]], [[2, 4], [4, 4], [2, 7]])
+
+
+determinante = algebra.det([[1, 3, 2], [4, 2, 4], [3, 1, 5]])
+print(determinante)
